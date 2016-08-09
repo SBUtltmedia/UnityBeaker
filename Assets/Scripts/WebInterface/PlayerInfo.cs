d4099e940f81e70e21643f959bc1c757eed1bb50
Michael Witkovsky <michael.witkovsky@gmail.com>
2016-08-09 18:21:53 -0400

    web test1

---
+++ Assets/Scripts/WebInterface/PlayerInfo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public string name;
+    public int lives;
+    public float health;
+
+    public PlayerInfo()
+    {
+        name = "Coconut Gun";
+        lives = 3;
+        health = 10;
+    }
+
+    public static PlayerInfo CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<PlayerInfo>(jsonString);
+    }
+
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    // Given JSON input:
+    // {"name":"Dr Charles","lives":3,"health":0.8}
+    // this example will return a PlayerInfo object with
+    // name == "Dr Charles", lives == 3, and health == 0.8f
+}
---
+++ Assets/Scripts/WebInterface/Web.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+
+public class Web : MonoBehaviour {
+
+    private PlayerInfo player;
+    private string json;
+
+    void Start()
+    {
+        player = new PlayerInfo();
+        StartCoroutine(UploadJSON());
+    }
+
+    IEnumerator UploadJSON()
+    {
+        string url = "https://apps.tlt.stonybrook.edu/SIMPLE_dev/resources/injest.php";
+        WWWForm form = new WWWForm();
+        json = player.SaveToString();
+        form.AddBinaryData("json", Encoding.ASCII.GetBytes(json), "unityJson.txt");
+        Debug.Log(form.data[0]);
+        WWW www = new WWW(url, form);
+
+        yield return www;
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            print(www.error);
+        }
+        else
+        {
+            print("Finished Uploading JSON");
+        }
+
+        Debug.Log(www);
+    }
+}
