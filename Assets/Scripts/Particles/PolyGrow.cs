5b5a59a9a632b585bd1f53f809d0a098273c39b3
Michael Witkovsky <michael.witkovsky@gmail.com>
2016-08-11 14:07:08 -0400

    Entering catcher beaker looks nicer now

--- Assets/Scripts/Particles/PolyGrow.cs
+++ Assets/Scripts/Particles/PolyGrow.cs
@@ -6,23 +6,19 @@ public class PolyGrow : MonoBehaviour {
     public GameObject topParticles;
 
     public float targetMaxY = 2.15f;
+    public int firstTheshold = 20;
     private int count = 0;
-	
-    void Start()
-    {
-        topParticles.SetActive(false);
-    }
 
 	public void ConsumeParticle()
-    {
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + (targetMaxY / 250.0f), transform.localScale.z);
-        transform.position = new Vector3(transform.position.x, transform.position.y + (targetMaxY / 500.0f), transform.position.z);
-        
-        if(++count == 10)
+    {        
+        if(++count > firstTheshold)
         {
-            topParticles.SetActive(true);
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - (10.0f*targetMaxY / 250.0f), transform.localScale.z);
-            transform.position = new Vector3(transform.position.x, transform.position.y - (10.0f*targetMaxY / 500.0f), transform.position.z);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + (targetMaxY / 250.0f), transform.localScale.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y + (targetMaxY / 500.0f), transform.position.z);
+        }
+        else
+        {
+            topParticles.transform.localScale = new Vector3(topParticles.transform.localScale.x + (0.25f / firstTheshold), topParticles.transform.localScale.y + (1.0f / firstTheshold), topParticles.transform.localScale.z);
         }
     }
 
